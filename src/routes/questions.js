d5537ee2a596a92d1e249ccdea3110dcd5f18292
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-05-25 15:37:57 -0300

    refactor: tirar comentários e exemplos hello world

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,11 +1,6 @@
 import express from 'express'
-import * as User from '../models/User.js'
-
-/* Implementar Model de dúvidas
-    import * as Question from '../models/Question.js'
-
-    
-*/
+import * as Question from '../models/Question.js'
+import { authenticate } from '../middleware/auth.js'
 
 const router = express.Router()
 
@@ -17,14 +12,38 @@ router.get('/:id', async (req, res) => {
     const { id } = req.params
     try {
         const question = await Question.findById(id)
-        if (!question) {
+
+        if (!question) 
             return res.status(404).json({ error: 'Question not found' })
-        }
+        
+        res.json({ question })
     }catch (error) {
         return res.status(500).json({ error: 'Error fetching question' })
     }
 
-    res.json({ question })
+})
+
+router.post('/', authenticate, async (req, res) => {
+
+    const { title, content, userId } = req.body
+    // const userId = req.user.id
+
+    if (!title || !content) {
+        return res.status(400).json({ error: 'Title and content are required' })
+    }
+
+    try {
+        const newQuestion = {
+            title,
+            content,
+            "user_id": userId
+        }
+        
+        await Question.createQuestion(newQuestion)
+        res.status(201).json(newQuestion)
+    } catch (error) {
+        res.status(500).json({ error: 'Error creating question' })
+    }
 })
 
 export default router
