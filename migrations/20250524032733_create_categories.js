d539b5119d5cd85f1cc040a32b880db5ad2fc095
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-05-25 15:36:00 -0300

    refactor: colocar o campo description da tabela de categorias como unique, e checar o tamanho de dados (até 255)

--- migrations/20250524032733_create_categories.js
+++ migrations/20250524032733_create_categories.js
@@ -2,10 +2,10 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+export const up = function(knex) {
     return knex.schema.createTable("categories", (table) => {
         table.uuid("id").primary();
-        table.text("description").notNullable();
+        table.string("description", 255).notNullable().unique().checkLength("<", 256);
         table.timestamp("created_at").defaultTo(knex.fn.now());
         table.timestamp("updated_at").defaultTo(knex.fn.now());
     });
@@ -15,6 +15,6 @@ exports.up = function(knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+export const down = function(knex) {
   return knex.schema.dropTable("categories");
 };
