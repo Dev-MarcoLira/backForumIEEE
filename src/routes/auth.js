d9a2aba286c65ad193bb1ebd5d4e43b9a7639571
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-05-25 00:33:16 -0300

    feat: add v4 uuid; add rotas admin, respostas

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import * as User from '../models/User.js'
+import { v4 } from 'uuid'
 
 const router = express.Router()
 
@@ -23,9 +24,20 @@ router.post('/register', async (req, res) => {
     const { username, password, role } = req.body
     const hashedPassword = await bcrypt.hash(password, 10)
 
+    const user = {
+        username,
+        password: hashedPassword,
+        role: role || 'user',
+        id: v4() // Generate a unique ID for the user
+    }
+
     try {
-        const user = await User.createUser({ username, password: hashedPassword, role })        
-        res.status(201).json({ id: user.id, username: user.username, role: user.role })
+        const userCreated = await User.createUser(user)        
+        res.status(201).json({ 
+            id: userCreated.id, 
+            username: userCreated.username,
+            role: 'user' 
+        })
     } catch (error) {
         res.status(500).json({ error: 'Error creating user' })
     }
@@ -48,7 +60,7 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ error: 'Invalid credentials' })
         
         const token = jwt.sign(
-            { id: user.id, role: user.role, username: user.username }, 
+            { id: user.id, role: user.role, username: user.username, role: user.role }, 
             process.env.JWT_SECRET, 
             { expiresIn: '1h' }
         )
@@ -58,9 +70,5 @@ router.post('/login', async (req, res) => {
     }
 
 })
-
-// Logout route
-
-
 
 export default router
--- src/server.js
+++ src/server.js
@@ -5,9 +5,10 @@ import express from "express";
 import cors from "cors";
 
 import defaultRoutes from "./routes/default.js";
-import protectedRoutes from "./routes/protected.js";
+import adminRoutes from "./routes/admin.js";
 import accountRoutes from "./routes/account.js";
 import questionRoutes from "./routes/questions.js";
+import repliesRoutes from "./routes/replies.js";
 import authRoutes from "./routes/auth.js";
 
 const PORT = process.env.PORT || 3000;
@@ -18,16 +19,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use("/api", defaultRoutes);
 app.use("/api/duvidas", questionRoutes);
-
-// Authentication Routes
+app.use("/api/respostas", repliesRoutes);
 app.use("/api/auth", authRoutes);
 
 // Protected Routes
 
 app.use("/api/conta", accountRoutes)
-app.use("/api/admin", protectedRoutes)
+app.use("/api/admin", adminRoutes)
 
 app.listen(PORT, 
     () => console.log(`Server is running on port ${PORT}`))
