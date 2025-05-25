454b466cbf6a87e9bbd9ed6b242ccf2a7c4b4e37
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-05-25 14:53:00 -0300

    refactor: adicionar comentário declarando que é uma rota hello-world (considerar apenas para testar se a API está funcionando)

--- src/routes/default.js
+++ src/routes/default.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
+/* 
+hello world route
+This route is used to check if the API is working 
+*/
 router.get('/', (req, res) => {
     res.json({ message: 'API is working' });
 });
