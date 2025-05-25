b3e4156fc80ccce4a97e216137d098a3d7af6db0
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-05-25 00:32:14 -0300

    change: arquivo protected alterado para admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,10 +4,6 @@ import { authenticate, requireRole } from '../middleware/auth.js';
 
 const router = express.Router();
 
-// router.get('/api', authenticate, (req, res) =>{
-//     res.json({ message: 'Protected route accessed!' });
-// })
-
 router.get('/', authenticate, requireRole('admin'), (req, res) => {
     res.json({ message: 'Admin route accessed!' });
 })
