7f5c6ef3d71fa260a759fa8d07e9ef6ac1d433dc
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-06-03 00:35:58 -0300

    feat: add rotas de curtidas (na pasta de rotas)

---
+++ src/routes/questionsLikes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const LikesQuestions = require('../models/LikesQuestions');
+
+router.get('/:questionId', async (req, res) => {
+    const { questionId } = req.params;
+    try {
+        
+        const likes = LikesQuestions.findById(questionId)
+
+        if (!likes) {
+            return res.status(404).json({ error: 'No likes found for this question' });
+        }
+
+        res.json({ likes });
+    } catch (error) {
+        return res.status(500).json({ error: error.message || 'Error fetching likes for question' });
+    }
+});
+
+router.delete('/:questionId', async (req, res) => {
+    const { questionId } = req.params;
+    const userId = req.user.id;
+
+    if(!questionId) {
+        return res.status(400).json({ error: 'Question ID is required' });
+    }
+
+    try {
+        const rowsAffected = await LikesQuestions.deleteLike(questionId, userId);
+
+        if (rowsAffected === 0) {
+            return res.status(404).json({ error: 'Like not found' });
+        }
+
+        res.status(204).send();
+    } catch (error) {
+        return res.status(500).json({ error: error.message || 'Error deleting like' });
+    }
+});
+
+export default router
---
+++ src/routes/repliesLikes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const LikesReplies = require('../models/LikesReplies');
+
+router.get('/:replyId', async (req, res) => {
+    const { replyId } = req.params;
+    try {
+        
+        const likes = LikesReplies.findById(replyId)
+
+        if (!likes) {
+            return res.status(404).json({ error: 'No likes found for this Reply' });
+        }
+
+        res.json({ likes });
+    } catch (error) {
+        return res.status(500).json({ error: error.message || 'Error fetching likes for Reply' });
+    }
+});
+
+router.delete('/:replyId', async (req, res) => {
+    const { replyId } = req.params;
+    const userId = req.user.id;
+
+    if(!replyId) {
+        return res.status(400).json({ error: 'Reply ID is required' });
+    }
+
+    try {
+        const rowsAffected = await LikesReplies.deleteLike(replyId, userId);
+
+        if (rowsAffected === 0) {
+            return res.status(404).json({ error: 'Like not found' });
+        }
+
+        res.status(204).send();
+    } catch (error) {
+        return res.status(500).json({ error: error.message || 'Error deleting like' });
+    }
+});
+
+export default router
